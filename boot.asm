4d08b7f824e0fd06226fd96a28b8f6355258b7d1
sasha <sasha@Debian2.myguest.virtualbox.org>
2024-04-27 12:27:15 +0300

    Reading from disk; added Makefile

--- boot.asm
+++ boot.asm
@@ -16,22 +16,6 @@ times 33 db 0
 start:
     jmp 0x7C0:step2 ; this will make the CS (code segment) = 0x7C0
 
-; implement interrupt 0; this is a divide by 0 exception
-handle_zero:
-    mov ah, 0x0E
-    mov al, 'A' ; print A
-    mov bx, 0
-    int 0x10
-    iret
-
-; implement interrupt 1
-handle_one:
-    mov ah, 0x0E
-    mov al, 'V' ; print V
-    mov bx, 0
-    int 0x10
-    iret
-
 step2:
     cli ; clear interrupts
 
@@ -49,20 +33,32 @@ step2:
     ; ending critical section
     sti ; enable interrupts
 
-    ; add interrupts to the vector table
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7C0
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7C0
-
-    ; trigger a divide by 0 exception
-    mov ax, 0
-    div ax
-    ; trigger interrupt 1
-    int 1
-
-    mov si, message ; move the address of the label "message" into the SI reg
+; read sector into memory
+; see https://www.ctyme.com/intr/rb-0607.htm
+; AH = 02h
+    mov ah, 0x02
+; AL = number of sectors to read (must be nonzero)
+    mov al, 1 ; read one sector
+; CH = low eight bits of cylinder number
+    mov ch, 0
+; CL = sector number 1-63 (bits 0-5)
+    mov cl, 2 ; read sector 2 (sector numbers start at 1 for CHS)
+; high two bits of cylinder (bits 6-7, hard disk only)
+; DH = head number
+    mov dh, 0
+; DL = drive number (bit 7 set for hard disk) => automatically set when bios boots 
+; ES:BX -> data buffer
+    mov bx, buffer
+    int 0x13
+    jc error ; if the carry flag is set (there is an error in the interrupt)
+    mov si, buffer
     call print
+    jmp $
+
+error:
+    mov si, error_message
+    call print
+
     jmp $ ; jump to itself so we never return
 
 print:
@@ -87,8 +83,7 @@ print_char:
     
     ret ; return
 
-; db -> put to file the following bytes
-message: db 'Hello World!', 0
+error_message: db "Failed to load sector", 0
 
 ; fill at least 510 bytes of data
 times 510-($ - $$) db 0
@@ -96,3 +91,8 @@ times 510-($ - $$) db 0
 ; the remaining 2 bytes will be filled with 55 AA (little endian)
 ; these need to be at byte 511 and 512
 dw 0xAA55 ; dw will put 2 bytes(one word) into the file
+
+; set a label where to write from disk
+; this will reference RAM memory just past the boot sector
+; the BIOS just loads one sector for booting, to this is just a lable, it won't be loaded
+buffer:
